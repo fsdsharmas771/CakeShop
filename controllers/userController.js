45f2be441eda9d22a1f55909274fecc115485f79
Tarun Sharma <115355429+fsdsharmas771@users.noreply.github.com>
2023-10-05 00:25:03 +0530

    login function

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,3 +86,34 @@ export const register = async (req, res, next) => {
 
     sendToken(res, user, "Registered Successfully", 201, true);
 };
+
+export const logIn = async (req, res, next) => {
+    const { phone, password } = req.body;
+    if (!phone) {
+        return res.status(400).json({
+            success: false,
+            message: "Please Enter Phone Number"
+        })
+    }
+    if (!password) {
+        return res.status(400).json({
+            success: false,
+            message: "Please Enter Password"
+        })
+    }
+    let user = await User.findOne({ phone }).select("password");
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User Not Found With This Phone Number"
+        })
+    }
+    const match = await user.matchPassword(String(password).trim());
+    if (!match) {
+        return res.status(400).json({
+            success: false,
+            message: "Incorrect Password"
+        })
+    }
+    return sendToken(res, user, "Correct OTP, your are logged in", 201, false);
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 
-import { register } from "../controllers/userController.js";
+import { register, logIn } from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 // To register a new user
 router.route("/register").post(register);
+router.route("/login").post(logIn)
 
 
 
